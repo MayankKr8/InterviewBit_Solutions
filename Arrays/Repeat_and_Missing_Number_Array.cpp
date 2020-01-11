d80d1472fba30401ecc92d88a17b27d78a422b49
Mayank <mayank.kumar.cse15@iitbhu.ac.in>
2020-01-11 17:50:27 +0530

    https://www.interviewbit.com/problems/repeat-and-missing-number-array/

---
+++ Arrays/Repeat_and_Missing_Number_Array.cpp
@@ -0,0 +1,21 @@
+// We maintain two sum variables which are the sum of difference bwtween the ith entry in the array and ith natural number and sum of difference of square of the two.
+// Hence if r is the repeated number and m is the missing number we have "r-m" and "r2 -m2", using the two we obtain r and m
+
+vector<int> Solution::repeatedNumber(const vector<int> &A) {
+    int m,r;
+    long long sum =0,s_sum=0,add;
+    for(int i=0;i<A.size();i++)
+    {
+        sum += A[i]-i-1;
+        s_sum += (long long)A[i]*(long long)A[i] - (long long)(i+1)*(long long)(i+1);
+    }
+    add = s_sum/sum;
+    add += sum;
+    r  = add/2;
+    m = r - sum;
+    vector<int> ans;
+    ans.push_back(r);
+    ans.push_back(m);
+    return ans;
+    
+}
