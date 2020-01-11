fcbcccade9b4aeed3466f4783b25e35585e3cc1e
Mayank <mayank.kumar.cse15@iitbhu.ac.in>
2020-01-11 17:25:25 +0530

    https://www.interviewbit.com/problems/add-one-to-number/

---
+++ Arrays/Add_One_To_Number.cpp
@@ -0,0 +1,17 @@
+//The basic approach is to do what you did as a child while adding numbers on your notebook
+//One digit at a time from right to left, while maintaining a carry
+
+vector<int> Solution::plusOne(vector<int> &A) {
+    reverse(A.begin(),A.end());
+    int carry=1,adder=1;
+    for(int i=0;i<A.size();i++)
+    {
+        A[i] += carry;
+        carry = A[i]/10;
+        A[i] = A[i]%10;
+    }
+    if(carry) A.push_back(carry);
+    if(A.front()==0 && A.size()>1) A.erase(A.begin());
+    reverse(A.begin(),A.end());
+    return A;
+}
