2a1de593c4c416242b1347f0343a708810316b2e
mayank.kum <mayank.kum@media.net>
2020-10-10 11:08:22 +0530

    https://www.interviewbit.com/problems/prime-sum/

---
+++ Math/Prime_sum.cpp
@@ -0,0 +1,22 @@
+vector<int> Solution::primesum(int A) {
+    vector<int> ans;
+    for(int i=2;i<=A/2;i++)
+    {
+        int i_ = A-i;
+        int flag = 0;
+        for (int j =2;j<=sqrt(i);j++)
+        {
+            if(i%j==0) flag =1;
+        }
+        for (int j =2;j<=sqrt(i_);j++)
+        {
+            if(i%j==0) flag =1;
+        }
+        if(flag==0)
+        {
+            ans.push_back(i);
+            ans.push_back(i_);
+            return ans;
+        }
+    }
+}
